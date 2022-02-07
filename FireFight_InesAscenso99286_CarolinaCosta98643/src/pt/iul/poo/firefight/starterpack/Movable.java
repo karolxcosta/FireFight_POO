4f15d6e7ec578eeff10e5bbe23aa1ae042ad2e76
karolxcosta <68022383+karolxcosta@users.noreply.github.com>
2022-02-07 18:03:50 +0000

    Delete FireFight_InesAscenso99286_CarolinaCosta98643 directory

--- FireFight_InesAscenso99286_CarolinaCosta98643/src/pt/iul/poo/firefight/starterpack/Movable.java
+++
@@ -1,8 +0,0 @@
-package pt.iul.poo.firefight.starterpack;
-
-//Interface que implementa as classes que têm movimento próprio
-public interface Movable {
-	
-	//Procedimento que implementa o movimento dos objetos que implementam esta interface
-	public void move(int keyCode);
-}
